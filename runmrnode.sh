682fddeca04c1afc4071fe6b13d3f74e28c62d74
MFMemon <mohammadfaizan91@gmail.com>
2024-10-25 05:47:41 +0400

    fixed position of args to the script

--- runmrnode.sh
+++ runmrnode.sh
@@ -4,4 +4,4 @@ DOWNLOAD_URL=https://github.com/MFMemon/mrp2pbin.git
 
 mkdir /tmp/mr
 git clone ${DOWNLOAD_URL} /tmp/mr/
-/tmp/mr/mrmaster --in=$1 --cc=$2 --nr=$3
+/tmp/mr/mrmaster --p=$1 --in=$2 --cc=$3 --nr=$4
