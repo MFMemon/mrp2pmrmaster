92dad08c69b8a4d836ecaae145ffbfee262ac634
MFMemon <mohammadfaizan91@gmail.com>
2024-10-30 01:27:30 +0400

    modified deployment script to add etcd client address

--- runmrnode.sh
+++ runmrnode.sh
@@ -4,4 +4,4 @@ DOWNLOAD_URL=https://github.com/MFMemon/mrp2pbin.git
 
 mkdir /tmp/mr
 git clone ${DOWNLOAD_URL} /tmp/mr/
-/tmp/mr/mrmaster --p=$1 --in=$2 --cc=$3 --nr=$4
+/tmp/mr/mrmaster --p=$1 --in=$2 --cc=$3 --nr=$4 --ec=$5 --id=$6
